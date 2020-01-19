b03ee04a0eb8b134c77f0845a5cc0e89d3d294e0
ALL-MIGHTY-LABT\karel <mirakl834@gmail.com>
2020-01-19 06:22:21 -0500

    extract text

--- server/server.py
+++ server/server.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import requests
 import base64
 from itertools import product
+import re
 
 from azure.storage.blob import BlockBlobService
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
@@ -22,7 +23,8 @@ bus_service = ServiceBusService(
 
 account_name = 'meganoni'
 account_key = 'dqODmqRYtyXC1skyDa8VmsY9Hupc+pQQp/OyKZFEcFU4yO1qZXPoW8BiOuZLJwldVo7G724NhIL3jyRpeAUgjA=='
-container_name = 'test'
+context_container_name = 'test'
+plate_container_name = 'plate'
 
 block_blob_service = BlockBlobService(
     account_name=account_name,
@@ -155,7 +157,7 @@ def init_get_money_3():
                 msg_json.pop("LicensePlateImageJpg")
                 img_context = msg_json.pop("ContextImageJpg")
                 blob_name = plate_num + ".jpg"
-                block_blob_service.create_blob_from_bytes(container_name, blob_name, base64.decodestring(img_context))
+                block_blob_service.create_blob_from_bytes(context_container_name, blob_name, base64.decodestring(img_context))
 
                 blob_url = blob_url_template % blob_name
 
@@ -190,7 +192,7 @@ def init_get_money_2():
             msg_json.pop("LicensePlateImageJpg")
             img_context = msg_json.pop("ContextImageJpg")
             blob_name = plate_num + ".jpg"
-            block_blob_service.create_blob_from_bytes(container_name, blob_name, base64.decodestring(img_context))
+            block_blob_service.create_blob_from_bytes(context_container_name, blob_name, base64.decodestring(img_context))
 
             blob_url = blob_url_template % blob_name
 
@@ -204,20 +206,101 @@ def init_get_money_2():
             )
 
 
-@app.route("/analyzePlate", methods = ['GET'])
-def analyze_plate():
-    remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
+@app.route( "/initGetMoney4", methods=['GET'] )
+def init_get_money_4():
+    with open( 'daily_wanted.txt' ) as json_file:
+        data = json.load( json_file )
+        wanted_plates = data['plates']
 
-    description_results = computervision_client.describe_image(remote_image_url)
+    while True:
+        msg = bus_service.receive_subscription_message( 'licenseplateread', 'eG4y7VYFse8NvW53', peek_lock=False )
 
-    # Get the captions (descriptions) from the response, with confidence level
-    print("Description of remote image: ")
-    if len(description_results.captions) == 0:
-        print("No description detected.")
-    else:
-        for caption in description_results.captions:
-            print("'{}' with confidence {:.2f}%".format( caption.text, caption.confidence * 100 ))
-            return "success"
+        msg_json = json.loads( msg.body )
+
+        plate_num = msg_json['LicensePlate']
+        print("PLATE NUMBER :" + plate_num)
+
+        img_plate = msg_json.pop("LicensePlateImageJpg")
+        blob_name = plate_num + ".jpg"
+        block_blob_service.create_blob_from_bytes(plate_container_name, blob_name, base64.decodestring( img_plate ) )
+
+        plate_blob_url = blob_url_template % blob_name
+
+        fuzzy_plate_nums = generate_fuzzy_list(plate_num)
+
+        for fuzzy_plate_num in fuzzy_plate_nums:
+            print("FUZZY PLATE: " + fuzzy_plate_num)
+            if fuzzy_plate_num in wanted_plates:
+                msg_json['LicensePlate'] = fuzzy_plate_num
+                request_url = "https://licenseplatevalidator.azurewebsites.net/api/lpr/platelocation"
+                username = "equipe13"
+                password = "RTFragcan38P5h8j"
+
+                img_context = msg_json.pop("ContextImageJpg")
+                blob_name = plate_num + ".jpg"
+                block_blob_service.create_blob_from_bytes(context_container_name, blob_name, base64.decodestring(img_context))
+
+                blob_url = blob_url_template % blob_name
+
+                msg_json['ContextImageReference'] = blob_url
+                req_json = json.dumps(msg_json)
+                resp = requests.post( request_url, data=req_json, auth=(username, password) )
+                json_file.close()
+                return Response(
+                    req_json,
+                    status=resp.status_code
+                )
+
+        extract_plate_num = extract_text(plate_blob_url)
+        print("EXTRACT: " + extract_plate_num)
+
+        if extract_plate_num in wanted_plates:
+            request_url = "https://licenseplatevalidator.azurewebsites.net/api/lpr/platelocation"
+            username = "equipe13"
+            password = "RTFragcan38P5h8j"
+
+            img_context = msg_json.pop( "ContextImageJpg" )
+            blob_name = plate_num + ".jpg"
+            block_blob_service.create_blob_from_bytes(context_container_name, blob_name, base64.decodestring( img_context ) )
+
+            blob_url = blob_url_template % blob_name
+
+            msg_json['ContextImageReference'] = blob_url
+            req_json = json.dumps( msg_json )
+            resp = requests.post( request_url, data=req_json, auth=(username, password) )
+            json_file.close()
+            return Response(
+                req_json,
+                status=resp.status_code
+            )
+
+
+ocr_url = COMPUTER_VISION_ENDPOINT + "vision/v2.1/ocr"
+
+
+def extract_text(remote_image_url):
+    headers = {'Ocp-Apim-Subscription-Key': COMPUTER_VISION_SUBSCRIPTION_KEY}
+    params = {'language': 'en', 'detectOrientation': 'true'}
+    data = {'url': remote_image_url}
+    response = requests.post( ocr_url, headers=headers, params=params, json=data )
+    response.raise_for_status()
+
+    analysis = response.json()
+    # Extract the word bounding boxes and text.
+    line_infos = [region["lines"] for region in analysis["regions"]]
+    words = list()
+    for line in line_infos:
+        for word_metadata in line:
+            word_to_add = ""
+            for word_info in word_metadata["words"]:
+                word_to_add = word_to_add + word_info["text"]
+            words.append(word_to_add)
+
+    for word in words:
+        if bool(re.search('^(?=.*[A-Z0-9])(?=.*\d)[A-Z0-9\d]{6,}$', word)):
+            return word
+
+    return ""
 
 
 @app.route("/getWantedList", methods=['GET'])
